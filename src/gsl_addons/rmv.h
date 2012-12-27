bf96ed7a4168f674cdaa5a13c0a65b42575179db
fiedukow <fiedukow@gmail.com>
2012-12-27 10:56:47 +0100

    Added support for multivariate normal density.

---
+++ src/gsl_addons/rmv.h
@@ -0,0 +1,31 @@
+/***************************************************************************************
+ *  Multivariate Normal density function and random number generator
+ *  Multivariate Student t density function and random number generator
+ *  Wishart random number generator
+ *  Using GSL -> www.gnu.org/software/gsl
+ *
+ *  Copyright (C) 2006  Ralph dos Santos Silva
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  AUTHOR 
+ *     Ralph dos Santos Silva,  address@hidden
+ *     March, 2006       
+***************************************************************************************/
+int rmvnorm(const gsl_rng *r, const int n, const gsl_vector *mean, const gsl_matrix *var, gsl_vector *result);
+double dmvnorm(const int n, const gsl_vector *x, const gsl_vector *mean, const gsl_matrix *var);
+int rmvt(const gsl_rng *r, const int n, const gsl_vector *location, const gsl_matrix *scale, const int dof, gsl_vector *result);
+double dmvt(const int n, const gsl_vector *x, const gsl_vector *locationn, const gsl_matrix *scale, const int dof);
+int rwishart(const gsl_rng *r, const int n, const int dof, const gsl_matrix *scale, gsl_matrix *result);
