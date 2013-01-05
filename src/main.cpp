3f894b09a7770cb3e35d9e9ee571bc58a1df7387
fiedukow <fiedukow@gmail.com>
2013-01-05 23:59:32 +0100

    Changing some parameteres.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "PodgladPostepu.hpp"
 #include <evol/Evol.hpp>
 #include "FunctionValue.hpp"
 #include "MultiDimGauss.hpp"
@@ -9,15 +10,16 @@
 int main(int /*argc*/, char** /*argv*/)
 {
   std::vector<MyGaussDescription> mgds;
-  double g1ex[] = {0, 0};
+  double g1ex[] = {-2, -2};
   mgds.push_back(MyGaussDescription(1.0, 1.0, 2, g1ex));
-  double g2ex[] = {0, 2};
-  mgds.push_back(MyGaussDescription(0.6, 0.2, 2, g2ex));
+  double g2ex[] = {2, 2};
+  mgds.push_back(MyGaussDescription(0.6, 0.6, 2, g2ex));
   
   MultiDimGauss gauss(mgds);
   FunctionValue goal(200, gauss); 
   evol::SubjectPtr prototype((evol::Subject*) new MultiDimPoint(2, gauss));
-  evol::Population pop((FitnessFunction&) goal, prototype, 1000, 0.2, 2.0);
+  evol::Population pop((FitnessFunction&) goal, prototype, 2500, 0.2, 2.0);
+  pop.registerObserver( NObserverPtr( new PodgladPostepu() ) );
 
   pop.start()->print();
   return 0;
