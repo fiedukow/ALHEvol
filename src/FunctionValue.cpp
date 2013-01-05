23b737dffc162de5e762a8b1d7135c88034fb0af
fiedukow <fiedukow@gmail.com>
2013-01-05 22:03:11 +0100

    Added FitnessFunction implementation (FunctionValue)

---
+++ src/FunctionValue.cpp
@@ -0,0 +1,43 @@
+#include "FunctionValue.hpp"
+#include "MultiDimPoint.hpp"
+
+#include <gsl/gsl_vector.h>
+#include <iostream>
+
+using namespace evol;
+
+FunctionValue::FunctionValue(double value, const MultiDimGauss& ff)
+  : value(value),
+    fitnessFunction(ff)
+{}
+
+bool FunctionValue::operator>(const FitnessFunction& toCompare) const
+{
+  return (this->value > dynamic_cast<const FunctionValue&>(toCompare).value);
+}
+
+bool FunctionValue::operator==(const FitnessFunction& toCompare) const
+{
+  return (this->value == dynamic_cast<const FunctionValue&>(toCompare).value);
+}
+
+void FunctionValue::calculate(const Subject& toCalculate)
+{
+  const MultiDimPoint& subject = dynamic_cast<const MultiDimPoint&>(toCalculate);
+  gsl_vector* dimValues = gsl_vector_alloc(subject.getDimensionsCount());
+  for(int i = 0; i < subject.getDimensionsCount(); ++i)
+    gsl_vector_set(dimValues, i, subject.getDimensionValue(i));
+  
+  value = fitnessFunction.getValueForVector(dimValues);
+  gsl_vector_free(dimValues);
+}
+
+std::unique_ptr<FitnessFunction> FunctionValue::clone() const
+{
+  return std::unique_ptr<FitnessFunction>(new FunctionValue(*this));
+}
+
+void FunctionValue::print() const
+{
+  std::cout << "Wartosc funkcji: " << value << std::endl;
+}
--- src/FunctionValue.hpp
+++ src/FunctionValue.hpp
@@ -2,12 +2,23 @@
 
 #include <gsl_addons/rmv.h>
 #include <evol/Population.hpp>
+#include "MultiDimGauss.hpp"
 
 using namespace evol;
 
 class FunctionValue : public FitnessFunction
 {
-  FunctionValue();
-  FunctionValue(  
-}
+public:
+  FunctionValue(double value, const MultiDimGauss& ff);
+  
+  virtual bool operator>(const FitnessFunction& toCompare) const;
+  virtual bool operator==(const FitnessFunction& toCompare) const;
+  virtual void calculate(const Subject& toCalculate);
+  virtual std::unique_ptr< FitnessFunction > clone() const;
+  virtual void print() const;
+  
+private: 
+  double value;
+  const MultiDimGauss& fitnessFunction;
+};
 
