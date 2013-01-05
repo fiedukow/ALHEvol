b492fdc910b042031cc0631e1ed666c8467a3ba4
fiedukow <fiedukow@gmail.com>
2013-01-05 22:02:41 +0100

    Added includes to gsl_addons/rmv.h.

--- src/gsl_addons/rmv.h
+++ src/gsl_addons/rmv.h
@@ -24,6 +24,15 @@
  *     Ralph dos Santos Silva,  address@hidden
  *     March, 2006       
 ***************************************************************************************/
+#include <math.h>
+#include <gsl/gsl_sf_gamma.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_linalg.h>
+
 int rmvnorm(const gsl_rng *r, const int n, const gsl_vector *mean, const gsl_matrix *var, gsl_vector *result);
 double dmvnorm(const int n, const gsl_vector *x, const gsl_vector *mean, const gsl_matrix *var);
 int rmvt(const gsl_rng *r, const int n, const gsl_vector *location, const gsl_matrix *scale, const int dof, gsl_vector *result);
