2ad01d8c6d2915f75faf0c2bdad1feb36ca7fc54
fiedukow <fiedukow@gmail.com>
2013-01-05 23:08:13 +0100

    Implemented first version of main - it does something, but who knows what.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <evol/Evol.hpp>
+#include "FunctionValue.hpp"
+#include "MultiDimGauss.hpp"
+#include "PointValue.hpp"
+#include "MultiDimPoint.hpp"
 
 int main(int /*argc*/, char** /*argv*/)
 {
-  std::cout << "Just placeholder for now" << std::endl;
+  std::vector<MyGaussDescription> mgds;
+  double g1ex[] = {0, 0};
+  mgds.push_back(MyGaussDescription(1.0, 1.0, 2, g1ex));
+  double g2ex[] = {0, 2};
+  mgds.push_back(MyGaussDescription(0.6, 0.2, 2, g2ex));
+  
+  MultiDimGauss gauss(mgds);
+  FunctionValue goal(200, gauss); 
+  evol::SubjectPtr prototype((evol::Subject*) new MultiDimPoint(2, gauss));
+  evol::Population pop((FitnessFunction&) goal, prototype, 1000, 0.2, 2.0);
+
+  pop.start()->print();
   return 0;
 }
