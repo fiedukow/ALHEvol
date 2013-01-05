dcf8355c08249da61da8f50e7e66b7b4d298409a
fiedukow <fiedukow@gmail.com>
2013-01-05 20:08:31 +0100

    Added implementation of MultiDimPoint (subject).

---
+++ src/MultiDimPoint.cpp
@@ -0,0 +1,34 @@
+#include "MultiDimPoint.hpp"
+#include <evol/Chromosome.hpp>
+
+#include <iostream>
+
+MultiDimPoint::MultiDimPoint(int dimensionsCount)
+  : dimensionsCount(dimensionsCount)
+{
+  points.reserve(dimensionsCount);
+}
+
+void MultiDimPoint::setInitialValue()
+{
+  clearChromosomes();
+  for(int i = 0; i < dimensionsCount; ++i)
+    addChromosome(ChromosomePtr(new PointValue()));
+}
+
+SubjectPtr MultiDimPoint::clone() const
+{
+  SubjectPtr newMultiDimPoint(new MultiDimPoint(dimensionsCount));
+  for(int i = 0; i < dimensionsCount; ++i)
+    newMultiDimPoint->addChromosome(ChromosomePtr(new PointValue(points[i].getValue())));
+
+  return newMultiDimPoint;
+}
+
+void MultiDimPoint::print() const
+{
+  std::cout << "Punkt: ";
+  for(int i = 0; i < dimensionsCount; ++i)
+    std::cout << points[i].getValue() << ((i != dimensionsCount-1) ? ", " : ";");
+  std::cout << std::endl;
+}
--- src/MultiDimPoint.hpp
+++ src/MultiDimPoint.hpp
@@ -1,19 +1,29 @@
 #pragma once
 
+#include <vector>
+
 #include <evol/EvolException.hpp>
 #include <evol/Subject.hpp>
+
+#include "PointValue.hpp"
 
 using namespace evol;
 
 class MultiDimPoint : public Subject
 {
-    public:
-    virtual SubjectPtr crossWith(SubjectPtr &subject) const throw (SubjectCrossException);
-    virtual void mutate();
+  public:
+  MultiDimPoint(int dimensionsCount); 
 
-    virtual void setInitialValue();
-    virtual SubjectPtr clone() const;
+  virtual void setInitialValue();
+  virtual SubjectPtr clone() const;
 
-    virtual void print() const;
+  virtual void print() const;
+
+  private:
+  int dimensionsCount;
+  std::vector<PointValue> points;
+/*std::list<double> covarianceFactors;
+  std::list<double> heightFactors;
+  std::list<std::list<double>> expectedValues;*/
 };
 
